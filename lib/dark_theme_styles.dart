cca86a155a5bdc80acf43a678bea0626061ebb6b
Noel Fischer <noelfischer@gmx.ch>
2022-12-12 16:10:48 +0100

    update colors

--- lib/dark_theme_styles.dart
+++ lib/dark_theme_styles.dart
@@ -3,30 +3,32 @@ import 'package:flutter/material.dart';
 class Styles {
   static ThemeData themeDataLight(BuildContext context) {
     return ThemeData(
-      primarySwatch: Colors.blue,
-      primaryColor: Colors.white,
-      backgroundColor: const Color(0xffF1F5FB),
-      indicatorColor: const Color(0xffCBDCF8),
-      hintColor: const Color(0xffEECED3),
-      highlightColor: const Color(0xffFCE192),
-      hoverColor: const Color.fromARGB(255, 223, 223, 223),
-      focusColor: const Color(0xffA8DAB5),
-      disabledColor: Colors.grey,
-      secondaryHeaderColor: Colors.white12,
-      shadowColor: Colors.white60,
-      unselectedWidgetColor: Colors.black,
-      cardColor: Colors.white,
-      canvasColor: Colors.grey[50],
-      brightness: Brightness.light,
-      buttonTheme: Theme.of(context)
-          .buttonTheme
-          .copyWith(colorScheme: const ColorScheme.light()),
-      appBarTheme: const AppBarTheme(
-        elevation: 0.0,
-      ),
-      textSelectionTheme:
-          const TextSelectionThemeData(selectionColor: Colors.black54),
-    );
+        primarySwatch: Colors.blue,
+        primaryColor: const Color.fromARGB(255, 241, 241, 241),
+        backgroundColor: const Color.fromARGB(255, 241, 241, 241),
+        scaffoldBackgroundColor: const Color.fromARGB(255, 241, 241, 241),
+        indicatorColor: const Color(0xffCBDCF8),
+        hintColor: const Color(0xffEECED3),
+        highlightColor: const Color(0xffFCE192),
+        hoverColor: const Color.fromARGB(255, 223, 223, 223),
+        focusColor: const Color(0xffA8DAB5),
+        disabledColor: Colors.black54,
+        secondaryHeaderColor: Colors.white12,
+        shadowColor: Colors.grey,
+        unselectedWidgetColor: Colors.black,
+        cardColor: Colors.white,
+        canvasColor: Colors.grey[50],
+        brightness: Brightness.light,
+        buttonTheme: Theme.of(context)
+            .buttonTheme
+            .copyWith(colorScheme: const ColorScheme.light()),
+        appBarTheme: const AppBarTheme(
+          elevation: 0.0,
+        ),
+        textSelectionTheme:
+            const TextSelectionThemeData(selectionColor: Colors.black54),
+        colorScheme: ColorScheme.fromSwatch()
+            .copyWith(primary: const Color.fromARGB(255, 12, 105, 182)));
   }
 
   static ThemeData themeDataDark(BuildContext context) {
